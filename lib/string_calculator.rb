b66bade961061a57780ed09b9d36e29cf9b0109d
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-04-09 09:09:10 +0530

    finding out how many times add method has been called

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,11 @@
 class StringCalculator
 
+  def initialize
+    @call_count = 0
+  end
+
   def add(numbers)
+    @call_count += 1
     return 0 if numbers.strip.empty?
 
     delimiter_regex = /,|\n/
@@ -23,5 +28,9 @@ class StringCalculator
     raise "negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
 
     nums.reject { |n| n > 1000 }.sum
+  end
+
+  def get_called_count
+    @call_count
   end
 end
