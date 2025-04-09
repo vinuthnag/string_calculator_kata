667ff5a59cdfe78c7614305d22efc71e6eb41ae9
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-04-09 07:17:18 +0530

    sum of all the numbers that are passed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,9 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.new.add("1,2")).to eq(3)
   end
 
-  
+  it 'returns sum of all the comma separated numbers' do
+    expect(StringCalculator.new.add("1,2,3,4")).to eq(10)
+  end
 
 
 end
