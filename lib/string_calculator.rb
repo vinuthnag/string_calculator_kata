1007a31429d03c891698b570e28bb4579ebcdda3
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-04-09 07:19:52 +0530

    handles new line as a d valid delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
 
   def add(numbers)
     return 0 if numbers.empty?
+
+    numbers = numbers.gsub("\n", ',')
     numbers.split(',').map(&:to_i).sum
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.new.add("1,2,3,4")).to eq(10)
   end
 
+  it 'handles newline as a delimiter' do
+    expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+  end
+
 
 end
