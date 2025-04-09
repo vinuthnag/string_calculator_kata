660778b0ece53179ea01bd62fdb43a4a3e7d79c0
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-04-09 07:35:56 +0530

    supports different delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,12 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    if numbers.start_with?("//")
+      delim_line, numbers = numbers.split("\n", 2)
+      delim = delim_line[2]
+      numbers = numbers.gsub(delim, ',')
+    end
+  
     numbers = numbers.gsub("\n", ',')
     numbers.split(',').map(&:to_i).sum
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,5 +22,9 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.new.add("1\n2,3")).to eq(6)
   end
 
+  it 'supports different delimeters' do
+    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+  end
+
 
 end
